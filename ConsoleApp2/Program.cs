0d80c046bbad081983a5819e537fdc05260a9352
DiazMaklinCarl <marklindiaz36@gmail.com>
2023-05-28 08:43:55 +0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Analog Clock Angle Calculator");
+        Console.WriteLine("------------------------------");
+
+        // Get input from the user
+        Console.Write("Enter hours (0-12): ");
+        int hours = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter minutes (0-59): ");
+        int minutes = int.Parse(Console.ReadLine());
+
+        // Calculate the angle between the hour hand and minute hand
+        double hourAngle = 0.5 * (hours * 60 + minutes);
+        double minuteAngle = 6 * minutes;
+        double angle = Math.Abs(hourAngle - minuteAngle);
+
+        // Ensure the angle is the smaller angle between the two hands
+        if (angle > 180)
+        {
+            angle = 360 - angle;
+        }
+
+        // Display the result
+        Console.WriteLine($"The smaller angle between the hour hand and minute hand is: {angle} degrees");
+    }
+}
